3b2d8adb74651aaa265ea57574c76f5fbc71ccfe
Seb <sebwiggins@me.com>
2021-07-23 10:59:54 +0100

    heroku mastterrrr

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     // try... catch documentation:
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
     try {
-      const res = await fetch(`http://localhost:4000${endpoint}`);
+      const res = await fetch(`https://mighty-harbor-48877.herokuapp.com${endpoint}`);
       const body = await res.json();
       setMessage(body.message);
       setDigipetStats(body.digipet);
